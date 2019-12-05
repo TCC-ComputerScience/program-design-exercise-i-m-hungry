c18c66a114ac839a41e548deee0ee9f28af0a339
jenaessary <31054440+jenaessary@users.noreply.github.com>
2019-12-05 10:18:23 -0500

    Create Anon Shell Code .hpp

---
+++ Anon Shell Code .hpp
@@ -0,0 +1,10 @@
+#ifndef BATTLESHIP_H
+#define BATTLESHIP_H
+
+//The program will generate a battleship game board. The user will guess the computer-generated location of the ships, and the gameboard will change to show the results.
+struct battleship {
+  bool shipLocation //The true and false values of this array will determine where the ships will be placed on the 10x10 grid by random
+  char battleMap //This will print the entirety of the map and will change based on whether a ship has been hit or not
+  };
+  
+
